c291a7a0e4d9c363e0b15ac8d8db97b2777c461c
Guilherme Affonso <guilherme.c.affonso@gmail.com>
2023-06-04 23:26:30 +0900

    Deactivate mark on consecutive calls to SetMark

--- Commands/Marks/SetMarkCommand.cs
+++ Commands/Marks/SetMarkCommand.cs
@@ -16,6 +16,7 @@ namespace Microsoft.VisualStudio.Editor.EmacsEmulation.Commands
     /// activates the region, which is of course empty immediately after this command.
     /// With a prefix arg of 4, this command does pop-global-mark.  
     /// With a prefix arg of 16, this command pops the top mark and throws it away with no effect on the point.
+    /// When called consecutively toggles between activating and deactivating the region.
     /// 
     /// Keys: Ctrl+Space | Ctrl+Shift+2
     /// </summary>
@@ -25,12 +26,42 @@ namespace Microsoft.VisualStudio.Editor.EmacsEmulation.Commands
     {
         internal override void Execute(EmacsCommandContext context)
         {
+            // Record the current caret position to avoid triggering the
+            // consecutive operation logic after mouse operations.
+            int currentPosition = context.TextView.GetCaretPosition().Position;
+
             if (context.UniversalArgument == 4)
+            {
                 context.MarkSession.PopMark();
+            }
             else if (context.UniversalArgument == 16)
+            {
                 context.MarkSession.RemoveTopMark();
+            }
+            else if (context.Manager.LastExecutedCommand != null &&
+                context.Manager.LastExecutedCommand.Command == (int)EmacsCommandID.SetMark &&
+                currentPosition == this.originalCaretPosition)
+            {
+                if (context.MarkSession.IsActive)
+                {
+                    context.MarkSession.Deactivate();
+                    context.Manager.UpdateStatus("Mark deactivated");
+                }
+                else
+                {
+                    context.MarkSession.Activate();
+                    context.Manager.UpdateStatus("Mark activated");
+                }
+            }
             else
+            {
+                this.originalCaretPosition = currentPosition;
                 context.MarkSession.PushMark();
+                context.Manager.UpdateStatus("Mark set");
+            }
         }
+
+        // Initial caret position during consecutive operations
+        internal int originalCaretPosition;
     }
 }
