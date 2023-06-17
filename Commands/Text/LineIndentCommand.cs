b584456d10c52812a8d2e6ac9ca3bac68a7b1c58
Guilherme Affonso <guilherme.c.affonso@gmail.com>
2023-06-17 15:26:59 +0900

    Move operations in LineIndentCommand to separate methods

--- Commands/Text/LineIndentCommand.cs
+++ Commands/Text/LineIndentCommand.cs
@@ -23,7 +23,6 @@ namespace Microsoft.VisualStudio.Editor.EmacsEmulation.Commands
         internal override void Execute(EmacsCommandContext context)
         {
             ITextSelection selection = context.TextView.Selection;
-            var position = context.TextView.CreateTrackingPoint();
             var line = context.EditorOperations.GetCaretPhysicalLine();
 
             // Return immediately if the buffer is read-only.
@@ -41,43 +40,77 @@ namespace Microsoft.VisualStudio.Editor.EmacsEmulation.Commands
 
                 if (selectionSpan.Start.Position.GetContainingLine().LineNumber != selectionSpan.End.Position.GetContainingLine().LineNumber)
                 {
-                    DTE vs = context.Manager.ServiceProvider.GetService<DTE>();
-                    vs.ExecuteCommand("Edit.FormatSelection");
-                    context.MarkSession.Deactivate();
-
-                    // After executing the Edit.FormatSelection command, the caret is always
-                    // placed at the end of the selection. Move it back to the starting point.
-                    context.EditorOperations.MoveCaret(position.GetPosition(context.TextView.TextSnapshot));
+                    IndentSelection(context);
                     return;
                 }
                 context.MarkSession.Deactivate();
             }
 
+            // If the line is empty, add indentation. Otherwise, use the Edit.FormatSelection
+            // command to set the right indentation value and type (spaces or tabs).
             if (String.IsNullOrWhiteSpace(line.GetText()))
             {
-                int? indentation = context.Manager.SmartIndentationService.GetDesiredIndentation(context.TextView, line);
-                context.EditorOperations.Delete(line.Extent);
-                if (indentation.HasValue)
-                {
-                    var startPosition = context.TextView.GetCaretPosition().GetContainingLine().Start.Position;
-                    context.TextBuffer.Insert(startPosition, new String(' ', indentation.Value));
-                    context.EditorOperations.MoveCaret(startPosition + indentation.Value);
-                }
-                else
-                {
-                    // We couldn't find any indentation level for the line, try the Indent command as the last resort
-                    context.EditorOperations.Indent();
-                }
-
-                // Finally, are any tab/spaces conversions necessary?
-                if (!context.TextView.Options.IsConvertTabsToSpacesEnabled())
-                {
-                    position = context.TextView.CreateTrackingPoint(PointTrackingMode.Positive);
-                    context.EditorOperations.ConvertSpacesToTabs();
-                    context.EditorOperations.MoveCaret(position.GetPosition(context.TextView.TextSnapshot));
-                }
-                return;
+                IndentEmptyLine(context);
             }
+            else
+            {
+                IndentLine(context);
+            }
+        }
+
+        /// <summary>
+        ///Indent multi-line selection
+        /// </summary>
+        private void IndentSelection(EmacsCommandContext context)
+        {
+            DTE vs = context.Manager.ServiceProvider.GetService<DTE>();
+            var position = context.TextView.CreateTrackingPoint();
+
+            vs.ExecuteCommand("Edit.FormatSelection");
+            context.MarkSession.Deactivate();
+
+            // After executing the Edit.FormatSelection command, the caret is always
+            // placed at the end of the selection. Move it back to the starting point.
+            context.EditorOperations.MoveCaret(position.GetPosition(context.TextView.TextSnapshot));
+        }
+
+
+        /// <summary>
+        /// Insert proper indentation on an empty line
+        /// </summary>
+        /// <param name="context"></param>
+        private void IndentEmptyLine(EmacsCommandContext context)
+        {
+            var line = context.EditorOperations.GetCaretPhysicalLine();
+            int? indentation = context.Manager.SmartIndentationService.GetDesiredIndentation(context.TextView, line);
+            context.EditorOperations.Delete(line.Extent);
+            if (indentation.HasValue)
+            {
+                var startPosition = context.TextView.GetCaretPosition().GetContainingLine().Start.Position;
+                context.TextBuffer.Insert(startPosition, new String(' ', indentation.Value));
+                context.EditorOperations.MoveCaret(startPosition + indentation.Value);
+            }
+            else
+            {
+                // We couldn't find any indentation level for the line, try the Indent command as the last resort
+                context.EditorOperations.Indent();
+            }
+
+            // Finally, are any tab/spaces conversions necessary?
+            if (!context.TextView.Options.IsConvertTabsToSpacesEnabled())
+            {
+                var position = context.TextView.CreateTrackingPoint(PointTrackingMode.Positive);
+                context.EditorOperations.ConvertSpacesToTabs();
+                context.EditorOperations.MoveCaret(position.GetPosition(context.TextView.TextSnapshot));
+            }
+        }
+
+        /// <summary>
+        /// Correct the indentation of the current line
+        /// </summary>
+        private void IndentLine(EmacsCommandContext context)
+        {
+            var position = context.TextView.CreateTrackingPoint();
 
             // SmartIndentationService seldom returns a value, and is not
             // smart enough to distinguish between tabs and spaces based
@@ -85,7 +118,7 @@ namespace Microsoft.VisualStudio.Editor.EmacsEmulation.Commands
             context.CommandRouter.ExecuteDTECommand("Edit.FormatSelection");
 
             // Recalculate the new indent position
-            line = context.EditorOperations.GetCaretPhysicalLine();
+            var line = context.EditorOperations.GetCaretPhysicalLine();
             var indentPosition = context.EditorOperations.GetNextNonWhiteSpaceCharacter(line.Start);
 
             // Move the caret forward when needed
